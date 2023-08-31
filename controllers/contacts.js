853ece5ad6d02dd55f3ae8746b23c61c2d0b6497
HannaTverdokhleb <1annakiss92@gmail.com>
2023-08-31 18:47:44 +0400

    separate routes

---
+++ controllers/contacts.js
@@ -0,0 +1,81 @@
+const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../models/contacts')
+const contactSchema = require('../schemas/contact')
+
+const routeGetAll = async (_, res, next) => {
+    try {
+      const list = await listContacts()
+      res.status(200).json(list)
+    } catch(err) {
+      next(err)
+    }
+}
+
+const routeGetById = async (req, res, next) => {
+    try {
+      const id = req.params.contactId
+      const contact = await getContactById(id)
+      if (contact === null) {
+        res.status(404).json({ message: 'Not found' })
+      } else {
+        res.status(200).json(contact)
+      }
+    } catch(err) {
+      next(err)
+    }
+}
+
+const routePostContact = async (req, res, next) => {
+    try {
+      const { error, value } = contactSchema.validate(req.body)
+      if (typeof error !== "undefined") {
+        res.status(400).send(error.details[0].message)
+      } else {
+        const { name, email, phone } = value
+        const contact = await addContact(name, email, phone)
+        res.status(201).send(contact)
+      }
+    } catch(err) {
+      next(err)
+    }
+  }
+
+const routeDeleteContact = async (req, res, next) => {
+    try {
+      const id = req.params.contactId
+      const contact = await removeContact(id)
+      if (contact === null) {
+        res.status(404).json({ message: 'Not found' })
+      } else {
+        res.status(200).json({ message: 'Contact deleted' })
+      }
+    } catch(err) {
+      next(err)
+    }
+}
+
+const routePutContact = async (req, res, next) => {
+    try {
+      const id = req.params.contactId
+      const { error, value } = contactSchema.validate(req.body)
+      if (typeof error !== "undefined") {
+        res.status(400).send(error.details[0].message)
+      } else {
+        const contact = await updateContact(id, value)
+        if (contact === null) {
+          res.status(404).json({ message: 'Not found' })
+        } else {
+          res.status(200).send(contact)
+        }
+      }
+    } catch(err) {
+      next(err)
+    }
+}
+
+module.exports = {
+    routeGetAll,
+    routeGetById,
+    routePostContact,
+    routeDeleteContact,
+    routePutContact,
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,81 +1,18 @@
 const express = require('express')
-const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../../models/contacts')
-const contactSchema = require('../../schemas/contact')
+const { routeGetAll, routeGetById, routePostContact, routeDeleteContact, routePutContact } = require('../../controllers/contacts')
 
 
 const router = express.Router()
 
-router.get('/', async (_, res, next) => {
-  try {
-    const list = await listContacts();
-    console.log(list)
-    res.status(200).json(list)
-  } catch(err) {
-    next(err)
-  }
-})
+router.get('/', routeGetAll)
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId
-    const contact = await getContactById(id)
-    if (contact === null) {
-      res.status(404).json({ message: 'Not found' })
-    } else {
-      res.status(200).json(contact)
-    }
-  } catch(err) {
-    next(err)
-  }
-})
+router.get('/:contactId', routeGetById)
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error, value } = contactSchema.validate(req.body)
-    if (typeof error !== "undefined") {
-      res.status(400).send(error.details[0].message)
-    } else {
-      const { name, email, phone } = value
-      const contact = await addContact(name, email, phone)
-      console.log(contact)
-      res.status(201).send(contact)
-    }
-  } catch(err) {
-    next(err)
-  }
-})
+router.post('/', routePostContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId
-    const contact = await removeContact(id)
-    if (contact === null) {
-      res.status(404).json({ message: 'Not found' })
-    } else {
-      res.status(200).json({ message: 'Contact deleted' })
-    }
-  } catch(err) {
-    next(err)
-  }
-})
+router.delete('/:contactId', routeDeleteContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const id = req.params.contactId
-    const { error, value } = contactSchema.validate(req.body)
-    if (typeof error !== "undefined") {
-      res.status(400).send(error.details[0].message)
-    } else {
-      const contact = await updateContact(id, value)
-      if (contact === null) {
-        res.status(404).json({ message: 'Not found' })
-      } else {
-        res.status(200).send(contact)
-      }
-    }
-  } catch(err) {
-    next(err)
-  }
-})
+router.put('/:contactId', routePutContact)
+
 
 module.exports = router
